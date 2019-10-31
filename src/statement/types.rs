5c32607f303557fb801d0cca79b886f5d363de5f
Carl Fredrik Samson <cf@samson.no>
2019-10-31 20:43:55 +0100

    improved error message

--- src/statement/types.rs
+++ src/statement/types.rs
@@ -173,7 +173,7 @@ unsafe impl<'a> OdbcType<'a> for &'a str {
         let cow = unsafe { ::environment::DB_ENCODING }.decode(buffer).0;
         match cow {
             Borrowed(strref) => strref,
-            Owned(_string) => unimplemented!(),
+            Owned(_string) => panic!("Couldn't convert value to `&str`. Try `String` or `Cow<str>` instead."),
         }
     }
 
